dc6509fe1292fd26474b5bcdcdd6e0c91a5c76d3
Sinhika <sinhika@republicofnewhome.org>
2020-04-07 23:12:53 -0500

    add coments

--- src/main/java/mod/alexndr/simpleores/init/ModItems.java
+++ src/main/java/mod/alexndr/simpleores/init/ModItems.java
@@ -20,6 +20,17 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+/**
+ * Holds a list of all our {@link Item}s.
+ * Suppliers that create Items are added to the DeferredRegister.
+ * The DeferredRegister is then added to our mod event bus in our constructor.
+ * When the Item Registry Event is fired by Forge and it is time for the mod to
+ * register its Items, our Items are created and registered by the DeferredRegister.
+ * The Item Registry Event will always be called after the Block registry is filled.
+ * Note: This supports registry overrides.
+ *
+ * @author Sinhika, notes by Cadiboo.
+ */
 public final class ModItems 
 {
     public static final DeferredRegister<Item> ITEMS = 
