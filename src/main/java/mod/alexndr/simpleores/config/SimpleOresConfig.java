a8fc56502b966a3d0c3b04333fd4258b9c1db588
Cynthia Higginbotham <cynthia.c.higginboth.ctr@navy.mil>
2020-04-30 13:15:24 -0500

    added FlagCondition class to manage conditions in recipes

--- src/main/java/mod/alexndr/simpleores/config/SimpleOresConfig.java
+++ src/main/java/mod/alexndr/simpleores/config/SimpleOresConfig.java
@@ -1,9 +1,15 @@
 package mod.alexndr.simpleores.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import mod.alexndr.simpleores.api.config.ISimpleConfig;
 import net.minecraft.world.gen.placement.CountRangeConfig;
 
-public class SimpleOresConfig
+public class SimpleOresConfig implements ISimpleConfig
 {
+    private static Map<String, Boolean> flags = new HashMap<>();
+    
     public static boolean enableCopperOre;
     public static boolean enableTinOre;
     public static boolean enableMythrilOre;
@@ -21,5 +27,25 @@ public class SimpleOresConfig
     public static  int adamantium_veinsize = 4;
     public static  CountRangeConfig onyx_cfg;
     public static  int onyx_veinsize = 7;
+    
+    
+    @Override
+    public boolean getFlag(String n)
+    {
+        Boolean obj = flags.get(n);
+        return obj != null && obj;
+    }
+
+
+    @Override
+    public void clear() {
+        flags.clear();
+    }
+
+    @Override
+    public void putFlag(String n, boolean val)
+    {
+        flags.put(n, val);
+    }
 
 }  // end class SimpleOresConfig
