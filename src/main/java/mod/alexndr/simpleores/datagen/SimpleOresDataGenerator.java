2fb42cdcce64667fa77a1c5d0fe16aa7d2b95b3c
Sinhika <sinhika@republicofnewhome.org>
2020-09-15 16:54:43 -0500

    datagen stuff added to SimpleOres, testing armor set buildier

---
+++ src/main/java/mod/alexndr/simpleores/datagen/SimpleOresDataGenerator.java
@@ -0,0 +1,105 @@
+package mod.alexndr.simpleores.datagen;
+
+import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD;
+
+import java.util.function.Consumer;
+
+import mod.alexndr.simpleores.SimpleOres;
+import mod.alexndr.simpleores.api.config.FlagCondition;
+import mod.alexndr.simpleores.api.datagen.RecipeSetBuilder;
+import mod.alexndr.simpleores.config.SimpleOresConfig;
+import mod.alexndr.simpleores.init.ModItems;
+import mod.alexndr.simpleores.init.ModTags;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.data.RecipeProvider;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.conditions.ICondition;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+/**
+ * bundles up the GatherDataEvent handler and all the necessary data providers for
+ * data generation.
+ * @author Sinhika
+ */
+@EventBusSubscriber(modid = SimpleOres.MODID, bus = MOD)
+public class SimpleOresDataGenerator
+{
+
+    /**
+     * GatherDataEvent handler.
+     * @param event the GatherDataEvent.
+     */
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event)
+    {
+        DataGenerator gen = event.getGenerator();
+        if (event.includeServer())
+        {
+            gen.addProvider(new Recipes(gen));
+        }
+     } // end gatherData()
+
+    /** 
+     * RecipeProvider for SimpleMod.
+     * @author Sinhika
+     *
+     */
+    public static class Recipes extends RecipeProvider implements IConditionBuilder
+    {
+        private RecipeSetBuilder setbuilder;
+        
+        public Recipes(DataGenerator generatorIn)
+        {
+            super(generatorIn);
+            setbuilder = new RecipeSetBuilder(SimpleOres.MODID);
+        }
+
+        protected void registerRecipes(Consumer<IFinishedRecipe> consumer)
+        {
+            registerStorageRecipes(consumer);
+            registerMiscRecipes(consumer);
+            registerToolRecipes(consumer);
+            registerArmorRecipes(consumer);
+            registerFurnaceRecipes(consumer);
+        } // end registerRecipes() 
+        
+        protected void registerToolRecipes(Consumer<IFinishedRecipe> consumer)
+        {
+        } // end registerToolRecipes()
+        
+        protected void registerArmorRecipes(Consumer<IFinishedRecipe> consumer)
+        {
+            setbuilder.buildSimpleArmorSet(consumer, Ingredient.fromTag(ModTags.Items.INGOTS_COPPER), "copper", 
+                                           hasItem(ModItems.copper_ingot.get()), flag("copper_armor"));
+        } // end registerArmorRecipes()
+        
+        protected void registerStorageRecipes(Consumer<IFinishedRecipe> consumer)
+        {
+        } // end registerStorageRecipes()
+        
+        protected void registerMiscRecipes(Consumer<IFinishedRecipe> consumer)
+        {
+            
+        } // end registerMiscRecipes()
+
+        protected void registerFurnaceRecipes(Consumer<IFinishedRecipe> consumer)
+        {
+        } // end registerFurnaceRecipes()
+        
+        /**
+         * Builds an ICondition representing FlagCondition...
+         *
+         */
+        public ICondition flag(String name)
+        {
+            return new FlagCondition(SimpleOresConfig.INSTANCE, name, new ResourceLocation("simpleores:flag"));
+        }
+
+    } // end subclass SimpleModDataGenerator$Recipes.
+    
+} // end-class SimpleModDataGenerator
