790105d915c8686b4665e4db7473103feb7657ad
Sinhika <sinhika@republicofnewhome.org>
2020-10-11 12:14:26 -0500

    make ModItemTags provider static and actually invoke it
    (cherry picked from commit b70fb336d498d6b632eb9893d476006d8d745f0f)

--- src/main/java/mod/alexndr/simpleores/datagen/SimpleOresDataGenerator.java
+++ src/main/java/mod/alexndr/simpleores/datagen/SimpleOresDataGenerator.java
@@ -60,6 +60,7 @@ public class SimpleOresDataGenerator
         {
             gen.addProvider(new Recipes(gen));
             gen.addProvider(new ModBlockTags(gen, event.getExistingFileHelper()));
+            gen.addProvider(new ModItemTags(gen));
             gen.addProvider(new SimpleOresLootTableProvider(gen));
         }
      } // end gatherData()
@@ -183,7 +184,7 @@ public class SimpleOresDataGenerator
     /**
      * ItemTagsProvider for SimpleOres.
      */
-    public class ModItemTags extends ItemTagsProvider
+    public static class ModItemTags extends ItemTagsProvider
     {
 
         public ModItemTags(DataGenerator generatorIn)
