29d90db727b687df10e9e8fd4d2f07fcbcbe07d8
Sinhika <sinhika@republicofnewhome.org>
2020-10-11 14:17:12 -0500

    implemented Silent's Crusher recipes

--- src/main/java/mod/alexndr/simpleores/init/ModTags.java
+++ src/main/java/mod/alexndr/simpleores/init/ModTags.java
@@ -1,8 +1,6 @@
 package mod.alexndr.simpleores.init;
 
-import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.Tag;
 import net.minecraft.util.ResourceLocation;
@@ -17,6 +15,9 @@ public class ModTags
         public static final Tag<Item> INGOTS_MYTHRIL = forgeTag("ingots/mythril");
         public static final Tag<Item> INGOTS_ADAMANTIUM = forgeTag("ingots/adamantium");
         public static final Tag<Item> GEMS_ONYX = forgeTag("gems/onyx");
+
+        public static final Tag<Item> ORES_MYTHRIL = forgeTag("ores/mythril");
+        public static final Tag<Item> ORES_ADAMANTIUM = forgeTag("ores/adamantium");
         
         public static final Tag<Item> DUSTS_MYTHRIL = forgeTag("dusts/mythril");
         public static final Tag<Item> DUSTS_ADAMANTIUM = forgeTag("dusts/adamantium");
@@ -33,14 +34,5 @@ public class ModTags
         }
     } // end subclass Items
     
-    public static class Blocks 
-    {
-        public static final Tag<Block> ORES_MYTHRIL = forgeTag("ores/mythril");
-        public static final Tag<Block> ORES_ADAMANTIUM = forgeTag("ores/adamantium");
-       
-        private static Tag<Block> forgeTag(String name) {
-            return new BlockTags.Wrapper(new ResourceLocation("forge", name));
-        }
-    } // end subclass Blocks
     
 } // end class ModTags
