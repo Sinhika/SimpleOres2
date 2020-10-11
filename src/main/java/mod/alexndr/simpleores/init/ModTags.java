9dff27e49ad5388af3aa4600510a402c77344e67
Sinhika <sinhika@republicofnewhome.org>
2020-10-11 14:04:34 -0500

    expanded ModTags to include dusts, chunks, ore blocks
    (cherry picked from commit 2c8d576616a2c2ab83c64d527d8fecb19d712dc7)

--- src/main/java/mod/alexndr/simpleores/init/ModTags.java
+++ src/main/java/mod/alexndr/simpleores/init/ModTags.java
@@ -1,5 +1,6 @@
 package mod.alexndr.simpleores.init;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.tags.ITag;
 import net.minecraft.tags.ItemTags;
@@ -15,11 +16,29 @@ public class ModTags
         public static final ITag.INamedTag<Item> INGOTS_ADAMANTIUM = forgeTag("ingots/adamantium");
         public static final ITag.INamedTag<Item> GEMS_ONYX = forgeTag("gems/onyx");
         
-        private static ITag.INamedTag<Item> forgeTag(String name) {
-            return ItemTags.makeWrapperTag("forge:" + name);
+        public static final ITag.INamedTag<Item> DUSTS_MYTHRIL = forgeTag("dusts/mythril");
+        public static final ITag.INamedTag<Item> DUSTS_ADAMANTIUM = forgeTag("dusts/adamantium");
+        
+        public static final ITag.INamedTag<Item> CHUNKS_MYTHRIL = silentsTag("chunks/mythril");
+        public static final ITag.INamedTag<Item> CHUNKS_ADAMANTIUM = silentsTag("chunks/adamantium");
+        
+        private static ITag.INamedTag<Item> silentsTag(String name) {
+            return new ItemTags.Wrapper(new ResourceLocation("silents_mechanisms", name));
         }
-    } // end class Items
+        
+        private static Tag<Item> forgeTag(String name) {
+            return new ItemTags.Wrapper(new ResourceLocation("forge", name));
+        }
+    } // end subclass Items
     
-    
+    public static class Blocks 
+    {
+        public static final ITag.INamedTag<Block> ORES_MYTHRIL = forgeTag("ores/mythril");
+        public static final ITag.INamedTag<Block> ORES_ADAMANTIUM = forgeTag("ores/adamantium");
+       
+        private static ITag.INamedTag<Block> forgeTag(String name) {
+            return new BlockTags.Wrapper(new ResourceLocation("forge", name));
+        }
+    } // end subclass Blocks
     
 } // end class ModTags
