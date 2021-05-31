684bb52608934b4e784f513948f183e3130a6167
Sinhika <sinhika@republicofnewhome.org>
2021-05-31 15:56:44 -0500

    fixed up config for refactor

--- src/main/java/mod/alexndr/simpleores/config/SimpleOresConfig.java
+++ src/main/java/mod/alexndr/simpleores/config/SimpleOresConfig.java
@@ -1,15 +1,10 @@
 package mod.alexndr.simpleores.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import mod.alexndr.simplecorelib.config.ISimpleConfig;
 import mod.alexndr.simplecorelib.config.ModOreConfig;
+import mod.alexndr.simplecorelib.config.SimpleConfig;
 
-public class SimpleOresConfig implements ISimpleConfig
+public class SimpleOresConfig extends SimpleConfig
 {
-    private static Map<String, Boolean> flags = new HashMap<>();
-    
     public static boolean enableCopperOre;
     public static boolean enableTinOre;
     public static boolean enableMythrilOre;
@@ -25,23 +20,5 @@ public class SimpleOresConfig implements ISimpleConfig
     public static ModOreConfig onyx_cfg;
     
     public static SimpleOresConfig INSTANCE = new SimpleOresConfig();
-    
-    @Override
-    public boolean getFlag(String n)
-    {
-        Boolean obj = flags.get(n);
-        return obj != null && obj;
-    }
-
-    @Override
-    public void clear() {
-        flags.clear();
-    }
-
-    @Override
-    public void putFlag(String n, boolean val)
-    {
-        flags.put(n, val);
-    }
 
 }  // end class SimpleOresConfig
