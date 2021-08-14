2cf0905560257b2fcb598f7eecf7a087f0ef4b4d
Sinhika <sinhika@republicofnewhome.org>
2021-08-14 10:49:15 -0500

    tier ResourceLocations must be lowercase

--- src/main/java/mod/alexndr/simpleores/content/SimpleOresTiers.java
+++ src/main/java/mod/alexndr/simpleores/content/SimpleOresTiers.java
@@ -35,23 +35,23 @@ public final class SimpleOresTiers
 	
 	public static final Tier COPPER = TierSortingRegistry.registerTier(
 			new ForgeTier(Tiers.STONE.getLevel(), 185, 4.0f, 1.0f, 8, COPPER_TAG,()->Ingredient.of(Items.COPPER_INGOT)),
-			new ResourceLocation(SimpleOres.MODID, "COPPER"), List.of(Tiers.WOOD), List.of(Tiers.IRON));
+			new ResourceLocation(SimpleOres.MODID, "copper"), List.of(Tiers.WOOD), List.of(Tiers.IRON));
 	
 	public static final Tier TIN = TierSortingRegistry.registerTier(
 			new ForgeTier(Tiers.STONE.getLevel(), 220, 3.5F, 1.0F, 8, TIN_TAG, ()->Ingredient.of( ModItems.tin_ingot.get())), 
-			new ResourceLocation(SimpleOres.MODID, "TIN"),List.of(Tiers.WOOD), List.of(Tiers.IRON));
+			new ResourceLocation(SimpleOres.MODID, "tin"),List.of(Tiers.WOOD), List.of(Tiers.IRON));
 	
 	public static final Tier MYTHRIL = TierSortingRegistry.registerTier(
 			new ForgeTier(Tiers.IRON.getLevel(), 800, 8.0F, 3.0F, 12, MYTHRIL_TAG, ()->Ingredient.of( ModItems.mythril_ingot.get())),  
-			new ResourceLocation(SimpleOres.MODID, "MYTHRIL"), List.of(Tiers.STONE), List.of(Tiers.DIAMOND));
+			new ResourceLocation(SimpleOres.MODID, "mythril"), List.of(Tiers.STONE), List.of(Tiers.DIAMOND));
 	
 	public static final Tier ADAMANTIUM = TierSortingRegistry.registerTier(
 			new ForgeTier(Tiers.IRON.getLevel(),1150, 14.0F, 3.0F, 3, ADAMANTIUM_TAG, ()->Ingredient.of( ModItems.adamantium_ingot.get())),
-			new ResourceLocation(SimpleOres.MODID, "ADAMANTIUM"), List.of(Tiers.IRON), List.of(Tiers.DIAMOND));
+			new ResourceLocation(SimpleOres.MODID, "adamantium"), List.of(Tiers.IRON), List.of(Tiers.DIAMOND));
 	
 	public static final Tier ONYX = TierSortingRegistry.registerTier(
 			new ForgeTier(Tiers.NETHERITE.getLevel(), 3280, 10.0F, 5.0F, 15, ONYX_TAG, ()->Ingredient.of( ModItems.onyx_gem.get())), 
-			new ResourceLocation(SimpleOres.MODID, "ONYX"),List.of(Tiers.DIAMOND), List.of() );
+			new ResourceLocation(SimpleOres.MODID, "onyx"),List.of(Tiers.DIAMOND), List.of() );
 
     
 } // end class
