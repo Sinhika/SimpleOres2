3774da080baaf009cc087f80c9a31c9e3833ecd6
Sinhika <sinhika@republicofnewhome.org>
2022-05-19 16:41:08 -0500

    updated to use fixed setBowProperties method from SimpleCoreLib, and new standardized FOVevent handler

--- src/main/java/mod/alexndr/simpleores/client/ClientForgeEventSubscriber.java
+++ src/main/java/mod/alexndr/simpleores/client/ClientForgeEventSubscriber.java
@@ -1,12 +1,10 @@
 package mod.alexndr.simpleores.client;
 
+import mod.alexndr.simplecorelib.api.client.ClientUtils;
 import mod.alexndr.simpleores.SimpleOres;
 import mod.alexndr.simpleores.config.SimpleOresConfig;
 import mod.alexndr.simpleores.content.MythrilBow;
 import mod.alexndr.simpleores.content.OnyxBow;
-import net.minecraft.world.item.BowItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.FOVModifierEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -29,33 +27,7 @@ public class ClientForgeEventSubscriber
     @SubscribeEvent
     public static void onFovEvent(FOVModifierEvent event) 
     {
-        float baseFOV = event.getFov();
-        float myNewFOV = 1.0F;
-        
-        ItemStack heldItemStack = event.getEntity().getMainHandItem();
-        if (heldItemStack.isEmpty()) { 
-            return;
-        }
-        Item heldItem = heldItemStack.getItem();
-        int useRemaining = event.getEntity().getTicksUsingItem();
-        if (heldItem instanceof BowItem)
-        { 
-            float zoom = 1.0F;
-            if (heldItem instanceof MythrilBow) 
-            {
-                zoom = SimpleOresConfig.mythrilBowZoom;
-            }
-            else if (heldItem instanceof OnyxBow) 
-            {
-                zoom = SimpleOresConfig.onyxBowZoom;
-            }
-            else {
-                return;
-            }
-            myNewFOV = baseFOV - (useRemaining * zoom / 20.0F);
-            if (myNewFOV < baseFOV - zoom)
-                myNewFOV = (baseFOV - zoom);
-            event.setNewfov(myNewFOV);
-        } 
+        ClientUtils.handleFovEvent(event, p -> p instanceof MythrilBow, SimpleOresConfig.mythrilBowZoom);
+        ClientUtils.handleFovEvent(event, p -> p instanceof OnyxBow, SimpleOresConfig.onyxBowZoom);
     } // end fovEvent
 } // end class
