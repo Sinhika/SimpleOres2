cb94a3e59ff787ba89194d85400378228ba71727
Sinhika <sinhika@republicofnewhome.org>
2024-07-05 14:46:18 -0500

    updated CreativeTabs init"

--- src/main/java/mod/alexndr/simpleores/init/CreativeTabs.java
+++ src/main/java/mod/alexndr/simpleores/init/CreativeTabs.java
@@ -5,8 +5,8 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.RegistryObject;
 
 public final class CreativeTabs
 {
@@ -14,22 +14,23 @@ public final class CreativeTabs
 	// formerly MOD_ITEM_GROUP
 	public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = 
 			DeferredRegister.create(Registries.CREATIVE_MODE_TAB, SimpleOres.MODID);
-	
-	public static final RegistryObject<CreativeModeTab> SIMPLEORES_TAB = CREATIVE_MODE_TABS.register("simplecore_tab",
+
+	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> SIMPLEORES_TAB
+			= CREATIVE_MODE_TABS.register("simpleores_tab",
 			() -> CreativeModeTab.builder()
-				.title(Component.translatable("item_group." + SimpleOres.MODID + ".simplecore_tab"))
-				.icon(() -> new ItemStack(ModItems.copper_pickaxe.get()))
-				.displayItems((parameters, output) -> {
-					output.acceptAll(ModBlocks.BLOCKS.getEntries().stream()
-										.map(RegistryObject::get)
-										.map(b -> (new ItemStack(b.asItem())))
-										.toList()
-										);
-					output.acceptAll(ModItems.ITEMS.getEntries().stream()
-							.map(RegistryObject::get)
-							.map(b -> (new ItemStack(b)))
-							.toList()
-							);
-				}).build());
+					.title(Component.translatable("item_group." + SimpleOres.MODID + ".simpleores_tab"))
+					.icon(() -> new ItemStack(ModItems.copper_pickaxe.get()))
+					.displayItems((parameters, output) -> {
+						output.acceptAll(ModBlocks.BLOCKS.getEntries().stream()
+								.map(DeferredHolder::get)
+								.map(b -> (new ItemStack(b.asItem())))
+								.toList()
+						);
+						output.acceptAll(ModItems.ITEMS.getEntries().stream()
+								.map(DeferredHolder::get)
+								.map(ItemStack::new)
+								.toList()
+						);
+					}).build());
 
 }  // end class ModTabGroup
